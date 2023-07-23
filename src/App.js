a5c90d0a143d3e4dd7e07c948cb3595e1f090ba3
LakshyaG34 <lakshyagoyal34@gmail.com>
2023-07-23 23:46:01 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+import './App.css';
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
 export class App extends Component {
+  state ={
+    progress:0
+  }
+  setProgress = (progress)=> {
+    this.setState({progress: progress})
+  }
   render() {
+    
     return (
       <div>
+        <Router>
         <Navbar/>
-        <News/>
-      </div>
+        <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}
+        
+      />
+        <Switch>
+        <Route exact path="/"><News setProgress={this.setProgress} key = "General" pageSize={this.pageSize} country="in" category="General"/></Route>
+        <Route exact path="/Business"><News setProgress={this.setProgress}key = "Business" pageSize={this.pageSize} country="in" category="Business"/></Route>
+        <Route exact path="/Health"><News setProgress={this.setProgress}key = "Health" pageSize={this.pageSize} country="in" category="Health"/></Route>
+        <Route exact path="/Science"><News setProgress={this.setProgress}key = "Science" pageSize={this.pageSize} country="in" category="Science"/></Route>
+        <Route exact path="/Education"><News setProgress={this.setProgress}key = "Education" pageSize={this.pageSize} country="in" category="Education"/></Route>
+        <Route exact path="/Sports"><News setProgress={this.setProgress}key = "Sports" pageSize={this.pageSize} country="in" category="Sports"/></Route>
+        <Route exact path="/Entertainment"><News setProgress={this.setProgress}key = "Entertainment" pageSize={this.pageSize} country="in" category="Entertainment"/></Route>
+        <Route exact path="/General"><News setProgress={this.setProgress}key = "General" pageSize={this.pageSize} country="in" category="General"/></Route>
+        </Switch>
+        </Router>
+      </div >
     );
   }
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,18 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageURL, newsURL}=this.props;
+    let { title, description, imageUrL, newsURL, author, date, source } =
+      this.props;
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
-  <img src={imageURL} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href={newsURL} target="_blank" className="btn btn-sm btn-primary">Read more</a>
-  </div>
-</div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              !imageUrL
+                ? "https://static.toiimg.com/thumb/msid-101684371,width-1070,height-580,imgsize-33266,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"
+                : imageUrL
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}
+              <span
+                class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+                style={{ left: "90" }}
+              >
+                {source}
+                <span class="visually-hidden">unread messages</span>
+              </span>
+            </h5>
+            <p className="card-text">{description}</p>
+            <a
+              href={newsURL}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read more
+            </a>
+            <p className="card-text">
+              By {!author ? "unknown" : author} published on{" "}
+              {new Date(date).toGMTString()}
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
