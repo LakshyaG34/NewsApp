90cfc2a18013fd0a2f9dac7f4a4b80a52a86d345
ishangarg9 <67002620+ishangarg9@users.noreply.github.com>
2023-07-26 18:27:52 +0530

    fix deployment

--- src/Components/News.js
+++ src/Components/News.js
@@ -52,9 +52,9 @@ const News = (props) => {
       </h1>
       {loading && articles && <Spinner />}
       <InfiniteScroll
-        dataLength={articles.length}
+        dataLength={articles?.length}
         next={fetchMoreData}
-        hasMore={articles.length !== totalResults}
+        hasMore={articles?.length !== totalResults}
         loader={<Spinner />}
       >
         <div className="container">
